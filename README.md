f8efd0b1cad98135a958d49c8f09700a44243217
Sven Bosau <sven@pythonandvba.com>
2024-01-10 21:07:58 +0100

    Initial Commit 🚀

---
+++ README.md
@@ -0,0 +1,26 @@
+# Video Suggestion App
+
+A simple web app to collect video ideas.
+The app connects to a no code tool called "Pabbly Connect" to further process/store the submitted data.
+
+## Video Tutorial (Coming soon!)
+[![YouTube Video](https://img.youtube.com/vi/XXX/0.jpg)](https://youtu.be/XXX)
+
+## Live Website
+[Link to Website](https://happy-holidays.streamlit.app/?name=Coding%20Is%20Fun%20Community)
+
+
+## 🤝 Connect with Me
+- 📺 **YouTube:** [CodingIsFun](https://youtube.com/c/CodingIsFun)
+- 🌐 **Website:** [PythonAndVBA](https://pythonandvba.com)
+- 💬 **Discord:** [Join the Community](https://pythonandvba.com/discord)
+- 💼 **LinkedIn:** [Sven Bosau](https://www.linkedin.com/in/sven-bosau/)
+- 📸 **Instagram:** [sven_bosau](https://www.instagram.com/sven_bosau/)
+
+## Support 
+If you appreciate the project and wish to encourage its continued development, consider [supporting my work](https://pythonandvba.com/coffee-donation).
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://pythonandvba.com/coffee-donation)
+
+## Feedback & Collaboration
+For feedback, suggestions, or potential collaboration opportunities, reach out at contact@pythonandvba.com.
+![Logo](https://www.pythonandvba.com/banner-img)
---
+++ streamlit_app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import requests
+
+WEBHOOK_URL = "https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NjUwNTY5MDYzMTA0MzA1MjZkNTUzZDUxMzci_pc"
+
+
+# Function to post data to the webhook using argument unpacking
+def post_to_webhook(**data):
+    response = requests.post(WEBHOOK_URL, json=data)
+    return response
+
+
+st.title("🎬 Submit Your Video Idea")
+
+st.markdown(
+    """
+Got a great idea for a video? I'd love to hear it! 🤔 Share your thoughts and help shape the content on 'Coding is Fun'.
+Check out my [YouTube channel](https://youtube.com/@codingisfun) for more.
+"""
+)
+
+with st.form(key="idea_form"):
+    name = st.text_input("Name (optional)", placeholder="Your Name")
+    email = st.text_input("Email (optional)", placeholder="Your Email")
+    video_idea = st.text_area("Video Idea", placeholder="Describe your idea...")
+
+    submit_button = st.form_submit_button(label="Submit Idea 🚀")
+
+    if submit_button:
+        if not video_idea.strip():
+            st.error("Please enter a video idea. 💡")
+            st.stop()
+
+        data = {"name": name, "email": email, "video_idea": video_idea}
+        response = post_to_webhook(**data)
+        if response.status_code == 200:
+            st.success("Thanks for your submission! 🌟")
+        else:
+            st.error("There was an error. Please try again. 🛠️")
