0aba61158556552ad9c4198fa77187030947ea4a
Sven <contact@pythonandvba.com>
2024-01-11 18:57:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-WEBHOOK_URL = "https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NjUwNTY5MDYzMTA0MzA1MjZkNTUzZDUxMzci_pc"
+WEBHOOK_URL = "https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NjUwNTY5MDYzMTA0MzI1MjY1NTUzMzUxMzci_pc"
 
 
 # Function to post data to the webhook using argument unpacking
